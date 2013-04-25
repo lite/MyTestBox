10ed9e22a1071cd373d35250a5060d098c41b79e
lite <litekok@gmail.com>
2013-04-25 14:02:24 +0800

    f.py forward http request, s.py setup a slim proxy

--- ruby/demo.rb
+++ ruby/demo.rb
@@ -1,10 +1,23 @@
 class Demo
   def transform(i)
-    return 64 if i<=64
+  	return do_transform_log2 i
+  	return 64 if i<=64
     return 128 if i<=128
     return 256 if i<=256
     return 512 if i<=512
     return 1024 if i<=1024
     2048
+  end
+
+  def do_transform(i)
+  	(i <= 64)? 64 :do_transform((i+1)/2) *2 
+  end
+
+  def do_transform_log2(i)
+  	1 << Math.log(i-1,2).to_i + 1
+  end
+
+  def my_logn(n)
+  	(n<2)?0:(my_logn(n/2)+1) 
   end
 end
--- ruby/demo_spec.rb
+++ ruby/demo_spec.rb
@@ -2,34 +2,41 @@ require "./demo"
 
 describe Demo do
 
+  before do
+    @demo = Demo.new
+  end
+
   it "should return 64 if <= 64" do
-  	demo = Demo.new
-   	demo.transform(63).should == 64
+  	@demo.transform(63).should == 64
   end
 
   it "should return 128 if >64 and <= 128" do
-  	demo = Demo.new
-   	demo.transform(65).should == 128
+  	@demo.transform(65).should == 128
   end
 
   it "should return 256 if >128 and <= 256" do
-  	demo = Demo.new
-   	demo.transform(210).should == 256
+  	@demo.transform(210).should == 256
   end
 
   it "should return 512 if >256 and <= 512" do
-  	demo = Demo.new
-   	demo.transform(257).should == 512
+  	@demo.transform(257).should == 512
   end
 
   it "should return 1024 if >512 and <= 1024" do
-  	demo = Demo.new
-   	demo.transform(1024).should == 1024
+  	@demo.transform(1024).should == 1024
   end
 
   it "should return 2048 if >1024 and <= 2048" do
-  	demo = Demo.new
-   	demo.transform(1025).should == 2048
+  	@demo.transform(1025).should == 2048
   end
 
+  it "should return 4096 if >2048 and <= 4096" do
+    @demo.transform(2049).should == 4096
+  end
+
+  it "should return Math.log" do
+    (1..100).each do |n|
+      @demo.my_logn(n).should == Math.log(n, 2).to_i
+    end
+  end
 end
